52df6ed392b39a2e6903b456323727a35af50bc3
Nick Pillitteri <nick@tshlabs.org>
2016-07-26 08:48:44 -0400

    Start of some tests

---
+++ tests/lib.rs
@@ -0,0 +1,99 @@
+extern crate staccato;
+
+use staccato::Statistics;
+
+
+const VALUES: &'static [f64] = &[
+    0.094f64,
+    0.111f64,
+    0.183f64,
+    0.197f64,
+    0.254f64,
+    0.271f64,
+    0.302f64,
+    0.366f64,
+    0.453f64,
+    0.494f64,
+    0.533f64,
+    0.542f64,
+    0.565f64,
+    0.568f64,
+    0.582f64,
+    0.595f64,
+    0.815f64,
+    0.912f64,
+    0.962f64,
+    0.974f64,
+];
+
+
+#[test]
+fn test_statistics_full_values_count() {
+
+}
+
+
+#[test]
+fn test_statistics_full_values_mean() {
+
+}
+
+
+#[test]
+fn test_statistics_full_values_upper() {
+
+}
+
+
+#[test]
+fn test_statistics_full_values_lower() {
+
+}
+
+
+#[test]
+fn test_statistics_full_values_median() {
+
+}
+
+
+#[test]
+fn test_statistics_full_values_stddev() {
+
+}
+
+
+#[test]
+fn test_statistics_75_values_count() {
+
+}
+
+
+#[test]
+fn test_statistics_75_values_mean() {
+
+}
+
+
+#[test]
+fn test_statistics_75_values_upper() {
+
+}
+
+
+#[test]
+fn test_statistics_75_values_lower() {
+
+}
+
+
+#[test]
+fn test_statistics_75_values_median() {
+
+}
+
+
+#[test]
+fn test_statistics_75_values_stddev() {
+
+}
